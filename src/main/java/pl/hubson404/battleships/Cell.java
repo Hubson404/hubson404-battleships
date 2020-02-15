b1bd08b664f589fbda5361f6e50ec82ee0262d72
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-02-15 15:59:55 +0100

    playable game, sets randomly single cell units

--- src/main/java/pl/hubson404/battleships/Cell.java
+++ src/main/java/pl/hubson404/battleships/Cell.java
@@ -8,6 +8,10 @@ public class Cell {
 
     private int status = 0;
 
+    public void markMiss(){
+        setStatus(2);
+    }
+
     public void placeShip(){
         setStatus(1);
     }
@@ -16,15 +20,23 @@ public class Cell {
         setStatus(-1);
     }
 
-    public void checkCell(){
+    public int cellState(){
+        int state = getStatus();
+        return state;
+
+    }
+
+    public void drawCell(){
         int cellStatus = getStatus();
 
         if (cellStatus == -1){
-            System.out.print("\\X/");
+            System.out.print(" X ");
         }else if (cellStatus == 0){
-            System.out.print(" ~ ");
+            System.out.print(" # ");
+        }else if (cellStatus == 1){
+            System.out.print(" S ");
         }else{
-            System.out.print("\\_/");
+            System.out.print(" 0 ");
         }
     }
 
